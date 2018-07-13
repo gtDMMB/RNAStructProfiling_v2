4033e63d4a87fe8551bc46f33377612b756ec426
Harrison Wilco <hwilco@gatech.edu>
2018-07-13 10:19:25 -0400

    Fix functionally similar group endpoint logic

--- Set.c
+++ Set.c
@@ -1642,23 +1642,33 @@ bool check_func_similar_stems(Set* set, Stem* stem1, Stem* stem2, int* freq) {
     int back1  = stem1->int_max_quad[1];
     int front2 = stem2->int_max_quad[0];
     int back2 = stem2->int_max_quad[1];
+    if (abs(front1 - front2) > FUNC_SIMILAR_END_DELTA || abs(back1-back2) > FUNC_SIMILAR_END_DELTA) {
+        return false;
+    }
+    /*
     if (!(front2 < back1 + FUNC_SIMILAR_END_DELTA &&
           front2 > front1 - FUNC_SIMILAR_END_DELTA) ||
         !(back2 < back1 + FUNC_SIMILAR_END_DELTA &&
           back2 > front1 - FUNC_SIMILAR_END_DELTA)) {
         return false;
     }
+     */
     // check for back portions
     front1 = stem1->int_max_quad[2];
     back1  = stem1->int_max_quad[3];
     front2 = stem2->int_max_quad[2];
     back2  = stem2->int_max_quad[3];
+    if (abs(front1 - front2) > FUNC_SIMILAR_END_DELTA || abs(back1-back2) > FUNC_SIMILAR_END_DELTA) {
+        return false;
+    }
+    /*
     if (!(front2 < back1 + FUNC_SIMILAR_END_DELTA &&
           front2 > front1 - FUNC_SIMILAR_END_DELTA) ||
         !(back2 < back1 + FUNC_SIMILAR_END_DELTA &&
           back2 > front1 - FUNC_SIMILAR_END_DELTA)) {
         return false;
     }
+     */
     if (set->opt->VERBOSE) {
         printf("Do stem %d and stem %d occur infrequently enough to be functionally similar: ", stem1->id,
                stem2->id);
