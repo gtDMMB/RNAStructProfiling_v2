677517e0f7117b44a6edf07067f951130cc73e0e
Harrison Wilco <hwilco@gatech.edu>
2018-07-19 13:19:30 -0400

    Modify update_output.py to allow updating of all output folders in one run

--- update_output.py
+++ update_output.py
@@ -43,6 +43,19 @@ def update_seq(seq_dir, src_dir):
    return 0
 
 
+def update_out_folder(out_folder_name):
+   out_folder_dir = output_dir + '/' + out_folder_name
+   os.chdir(out_folder_dir)
+   seq_names = sorted(next(os.walk('.'))[1])
+   for s in seq_names:
+      if update_seq(out_folder_dir + '/' + s, src_dir) == 0:
+         print('SUCCESS: ' + s)
+      else:
+         print('FAILURE: ' + s)
+   print('Done: ' +  out_folder_name)
+   return 0
+
+
 top_dir = os.path.dirname(os.path.realpath(__file__))
 src_dir = top_dir + '/src'
 if not os.path.isdir(src_dir):
@@ -54,24 +67,20 @@ if not os.path.isdir(output_dir):
    exit(1)
 os.chdir(output_dir)
 out_folder_names = sorted(next(os.walk('.'))[1])
-output_prompt = '\n'.join([str(x+1) + '. ' + (out_folder_names[x]).capitalize() for x in range(len(out_folder_names))]) + '\nSelect an output folder to update: '  
+output_prompt = '1. All\n' + '\n'.join([str(x+2) + '. ' + (out_folder_names[x]).capitalize() for x in range(len(out_folder_names))]) + '\nSelect an output folder to update: '  
 while True:
    try:
-      out_folder_choice = int(input(output_prompt)) - 1
+      out_folder_choice = int(input(output_prompt)) - 2
    except NameError:
       print 'Please enter a number\n'
    else:
-      if 0 <= out_folder_choice < len(out_folder_names):
+      if -1 <= out_folder_choice < len(out_folder_names):
          break
       else:
          print 'Out of range. Try again\n'
-out_folder_dir = output_dir + '/' + out_folder_names[out_folder_choice]
-os.chdir(out_folder_dir)
-seq_names = sorted(next(os.walk('.'))[1])
-for s in seq_names:
-   if update_seq(out_folder_dir + '/' + s, src_dir) == 0:
-      print('SUCCESS: ' + s)
-   else:
-      print('FAILURE: ' + s)
-print('Done')
+if out_folder_choice == -1:
+   for name in out_folder_names:
+      update_out_folder(name)
+else:
+   update_out_folder(out_folder_names[out_folder_choice])
 exit(0)
