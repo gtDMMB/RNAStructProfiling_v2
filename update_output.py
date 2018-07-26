959cf94b261adc07fce19e038fee4a32448a7e5a
Harrison Wilco <hwilco@gatech.edu>
2018-07-26 17:28:55 -0400

    Allow passing of arguments into update_output.py

--- update_output.py
+++ update_output.py
@@ -7,49 +7,59 @@
 import os
 import subprocess
 
-def update_seq(seq_dir, src_dir):
+def check_outputs(output_names, args):
+   temp = []
+   if '-g' not in args:
+      temp += [output_names[0]]
+   if '-sg' not in args:
+      temp += [output_names[1]]
+   return temp
+
+
+def update_seq(seq_dir, src_dir, args):
    os.chdir(seq_dir)
    seq_name = seq_dir[seq_dir.rfind('/')+1:]
    if not os.path.exists(seq_dir + '/input'):
       print('No input folder found for sequence: ' + seq_name)
-      return 1
    seq_file = seq_dir + '/input/' + seq_name + '.txt'
    if not os.path.isfile(seq_file):
       print('No sequence file found. [sequenceName].txt')
-      return 1
    sample_file = seq_dir + '/input/' + seq_name + '.out'
-   if not os.path.isfile(seq_file):
+   if not os.path.isfile(sample_file):
       print('No sample file found. [sequenceName].out')
+   if not os.path.exists(seq_dir + '/input') or not os.path.isfile(seq_file) or not os.path.isfile(seq_file):
       return 1
    old_path = seq_dir + '/old'
    if not os.path.exists(old_path):
       os.makedirs(old_path)
-   out_file_names = ['/out.dot','/out_consolidated.dot','/verbose.txt','/structure.out','/stem_structure.out','/helices.txt','/key.txt']
-   for f in out_file_names + ['/out.png', '/out_consolidated.png', '/comparison.txt']:
+   output_names = ['/out.dot','/out_consolidated.dot']
+   output_names = check_outputs(output_names, args)
+   out_file_names = ['/verbose.txt','/structure.out','/stem_structure.out','/helices.txt','/key.txt'] + output_names
+   for f in out_file_names + [x + '.png' for x in output_names] + ['/comparison.txt']:
       if os.path.isfile(seq_dir + f):
          os.rename(seq_dir + f, old_path + f)
    os.chdir(src_dir)
    for f in out_file_names:
       if os.path.isfile(src_dir + f):
          os.remove(src_dir + f)
-   subprocess.call('./RNAprofile -o out -v -sfold "' + sample_file + '" "' + seq_file + '" > verbose.txt', shell=True)
+   subprocess.call('./RNAprofile -o out -v ' + args + ' -sfold "' + sample_file + '" "' + seq_file + '" > verbose.txt', shell=True)
    for f in out_file_names:
       if os.path.isfile(src_dir + f):
          os.rename(src_dir + f, seq_dir + f)
       else:
-         print('ERROR: sequence ' + seq_name)
+         return 1
    os.chdir(seq_dir)
-   subprocess.call('dot -T png -o ' + out_file_names[0][1:-4] + '.png ' + out_file_names[0][1:] + ' > /dev/null', shell=True)
-   subprocess.call('dot -T png -o ' + out_file_names[1][1:-4] + '.png ' + out_file_names[1][1:] + ' > /dev/null', shell=True)
+   for f in output_names:
+      subprocess.call('dot -T png -o ' + f[1:-4] + '.png ' + f[1:] + ' > /dev/null', shell=True)
    return 0
 
 
-def update_out_folder(out_folder_name):
+def update_out_folder(out_folder_name, args):
    out_folder_dir = output_dir + '/' + out_folder_name
    os.chdir(out_folder_dir)
    seq_names = sorted(next(os.walk('.'))[1])
    for s in seq_names:
-      if update_seq(out_folder_dir + '/' + s, src_dir) == 0:
+      if update_seq(out_folder_dir + '/' + s, src_dir, args) == 0:
          print('SUCCESS: ' + s)
       else:
          print('FAILURE: ' + s)
@@ -68,6 +78,7 @@ if not os.path.isdir(output_dir):
    exit(1)
 os.chdir(output_dir)
 out_folder_names = sorted(next(os.walk('.'))[1])
+args = raw_input('Enter any aditional arguments to pass: ')
 output_prompt = '1. All\n' + '\n'.join([str(x+2) + '. ' + (out_folder_names[x]).capitalize() for x in range(len(out_folder_names))]) + '\nSelect an output folder to update: '  
 while True:
    try:
@@ -85,5 +96,5 @@ if out_folder_choice == -1:
    for name in out_folder_names:
       update_out_folder(name)
 else:
-   update_out_folder(out_folder_names[out_folder_choice])
+   update_out_folder(out_folder_names[out_folder_choice], args)
 exit(0)
